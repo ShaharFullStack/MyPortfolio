a3e7634bc24addc3ba746f165f71670d3565bf79
ShaharFullStack <shahargtr@gmail.com>
2024-11-30 14:44:05 +0200

    Updated files

--- responsive-navbar-2-main/assets/js/main.js
+++ responsive-navbar-2-main/assets/js/main.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /*=============== SHOW MENU ===============*/
 const navMenu = document.getElementById('nav-menu'),
       navToggle = document.getElementById('nav-toggle'),
@@ -17,23 +16,3 @@ if(navClose){
       navMenu.classList.remove('show-menu')
    })
 }
-=======
-/*=============== SHOW MENU ===============*/
-const navMenu = document.getElementById('nav-menu'),
-      navToggle = document.getElementById('nav-toggle'),
-      navClose = document.getElementById('nav-close')
-
-/* Menu show */
-if(navToggle){
-   navToggle.addEventListener('click', () =>{
-      navMenu.classList.add('show-menu')
-   })
-}
-
-/* Menu hidden */
-if(navClose){
-   navClose.addEventListener('click', () =>{
-      navMenu.classList.remove('show-menu')
-   })
-}
->>>>>>> 6d8723415d01a39c583ad2da33beef37cd1b20c6
