72a9de5faf0e8f9ee7d969c3e61645b80e80155f
ShaharFullStack <shahargtr@gmail.com>
2024-11-30 14:49:05 +0200

    Resolved issues and updated files

--- acco/acco.js
+++ acco/acco.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function openLightbox(src) {
     var modal = document.getElementById("lightboxModal");
     var modalImg = document.getElementById("lightboxImg");
@@ -10,16 +9,3 @@ function closeLightbox() {
     var modal = document.getElementById("lightboxModal");
     modal.style.display = "none";
 }
-=======
-function openLightbox(src) {
-    var modal = document.getElementById("lightboxModal");
-    var modalImg = document.getElementById("lightboxImg");
-    modal.style.display = "block";
-    modalImg.src = src;
-}
-
-function closeLightbox() {
-    var modal = document.getElementById("lightboxModal");
-    modal.style.display = "none";
-}
->>>>>>> 6d8723415d01a39c583ad2da33beef37cd1b20c6
